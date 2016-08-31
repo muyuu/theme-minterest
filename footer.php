a10a6444c169c652933b2e2bc8c1ac69dc37a5ac
muyuu <anticyborg@gmail.com>
2016-08-31 09:07:18 +0900

    apply standby.css layout class

--- footer.php
+++ footer.php
@@ -11,14 +11,18 @@
 
 ?>
 
+        	</div><!--.l-row-->
+	    </div><!--.inner-->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minterest' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'minterest' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'minterest' ), 'minterest', '<a href="http://minterest.site/" rel="designer">muyuu</a>' ); ?>
-		</div><!-- .site-info -->
+		<div class="container">
+            <div class="site-info">
+                <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minterest' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'minterest' ), 'WordPress' ); ?></a>
+                <span class="sep"> | </span>
+                <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'minterest' ), 'minterest', '<a href="http://minterest.site/" rel="designer">muyuu</a>' ); ?>
+            </div><!-- .site-info -->
+		</div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area col-md-8">
+	<div id="primary" class="content-area l-col8">
 		<main id="main" class="site-main" role="main">
 
 		<?php
